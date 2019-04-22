354d934f131dcba46281556b6cd98c2a148b77b6
Dakota Walsh <kotajacob@protonmail.com>
2019-04-21 21:01:35 -0400

    will now remove oldest files till under disk size
    It now enters a while loop for the disk usage percentage and deletes the
    oldest file until under the max usage percent.

--- remove.py
+++ remove.py
@@ -6,24 +6,27 @@ import os  # file operations
 import sys # system operations
 
 def sorted_ls(path):
+	# sort the web_dir by age
 	mtime = lambda f: os.stat(os.path.join(path, f)).st_mtime
 	return list(sorted(os.listdir(path), key=mtime))
 
 def main():
 	# public web directory with the images
 	web_dir = "/var/www/html/" # EDIT THIS FOR YOUR SERVER
-	max_disk_percent = 0.00 # Max disk usage percentage out of 1
+	max_disk_percent = 0.80 # Max disk usage percentage out of 1
 
 	# determine the current disk usage
 	stat  = os.statvfs(web_dir)
 	total = stat.f_blocks*stat.f_bsize
 	free  = stat.f_bfree*stat.f_bsize
 	used  = 1-(free/total)
-	# if it's less than the max disk percent exit
-	if (used < max_disk_percent):
-		sys.exit(0)
-
-	print(sorted_ls(web_dir))
+	# if it's more than the max disk percent remove oldest file
+	while (used > max_disk_percent):
+		os.remove(os.path.join(web_dir, sorted_ls(web_dir)[0]))
+		stat  = os.statvfs(web_dir)
+		total = stat.f_blocks*stat.f_bsize
+		free  = stat.f_bfree*stat.f_bsize
+		used  = 1-(free/total)
 
 if __name__ == '__main__':
 	main()
